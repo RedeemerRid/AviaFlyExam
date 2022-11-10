e9510d045772f67c60a02a1e64cb0aa14be477af
kavkaznalchik <akhmatovich@gmail.com>
2022-11-10 23:19:10 +0300

    edit 1

---
+++ Classes/mainOper.cpp
@@ -0,0 +1,22 @@
+#include "..\\Classes\Flight.h"
+#include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+
+int main() {
+
+	Flight f;
+	//f.addFlight();
+	//f.addFlight();
+	//f.addFlight
+	f.InToFlight();
+	f.OutToFile();
+	f.print();
+	f.editFlight();
+	f.print();
+
+
+
+	return 0;
+}
