bed37f23a28c4b8caf6d56e8713febc4b86a8f14
rid <akhmatovich@gmail.com>
2022-11-12 16:32:33 +0300

    bool exit 1

--- Classes/mainOper.cpp
+++ Classes/mainOper.cpp
@@ -6,20 +6,64 @@
 
 int main() {
 
+	bool exit = true;
+
+	int press;
+		
 	Flight f;
-	//f.addFlight();
-	//f.addFlight();
-	//f.addFlight
-	//f.InToFlight();
-	//f.OutToFile();
-	//f.print();
-	//f.editFlight();
-	//f.print();
-	//f.delFlight();
-	f.print();
-	f.addFlight();
-	f.delFlight();
-	f.print();
+
+	do
+
+	{
+		cout << "select  0  -  exit : " << endl;
+		cout << "select  1  -  add to flight : " << endl;
+		cout << "select  2  -  eddit to flight : " << endl;
+		cout << "select  3  -  delete to flight : " << endl;
+		cout << "select  4  -  viuwer to list passager in flight : " << endl;
+		
+
+		while (!isdigit(char(cin.peek())))
+
+		{
+
+			system("cls");
+
+			cin.ignore();
+
+			cout << "select  0  -  exit : " << endl;
+			cout << "select  1  -  add to flight : " << endl;
+			cout << "select  2  -  eddit to flight : " << endl;
+			cout << "select  3  -  delete to flight : " << endl;
+			cout << "select  4  -  viuwer to list passager in flight : " << endl;
+
+		}
+
+	    cin >> press;
+		switch (press) {
+			case 0:
+				cout << "if you want to exit? -- press -- 0, if you continue = press -- 1 : ";;
+				cin >> exit;
+				break;
+			case 1:
+			    f.addFlight();
+			    break;
+		    case 2:
+			    f.editFlight();
+			    break;
+		    case 3:
+			    f.delFlight();
+			    break;
+		    case 4:
+			    cout << "viuwer" << endl;
+			    break;
+		    //default:
+			    //break;
+		}
+	} while (exit != 0);
+	
+	
+	
+	
 
 
 
